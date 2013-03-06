f7050fc328d9f0a7c44386fb5d0c9cd7b4b9b3a5
Emanuele Aina <emanuele.aina@collabora.com>
2013-03-06 21:47:31 +0100

    chromium: Complete the lists of services we want to enable

--- chromium-extension/content.google.gdrive.js
+++ chromium-extension/content.google.gdrive.js
@@ -1,5 +1,5 @@
 console.log("goa: GOA content script for GDrive loading");
 setupGoaIntegration (
   '//*[starts-with(@href, "https://profiles.google.com/")]//text()[contains(.,"@")]',
-  ['files']
+  ['files', 'documents']
 );
