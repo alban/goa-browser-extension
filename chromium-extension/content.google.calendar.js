1b1230058390fac91c06cfd114ac9e47db1b1119
Alban Crequy <alban.crequy@collabora.co.uk>
2013-06-26 17:27:18 +0100

    fix login for registered users on Google+
    Changes on gdrive, gcalendar, picasa, gmail

--- chromium-extension/content.google.calendar.js
+++ chromium-extension/content.google.calendar.js
@@ -1,5 +1,5 @@
 console.log("goa: GOA content script for Google Calendar loading");
 setupGoaIntegration (
-  '//*[starts-with(@href, "https://profiles.google.com/")]//text()[contains(.,"@")]',
+  '//*[starts-with(@href,"https://plus.google.com/") or starts-with(@href,"https://profiles.google.com/")]/..//text()[contains(.,"@")]',
   ['calendar']
 );
--- chromium-extension/content.google.picasaweb.js
+++ chromium-extension/content.google.picasaweb.js
@@ -1,5 +1,5 @@
 console.log("goa: GOA content script for Picasa loading");
 setupGoaIntegration (
-  '//*[starts-with(@href, "https://profiles.google.com/")]//text()[contains(.,"@")]',
+  '//*[starts-with(@href,"https://plus.google.com/") or starts-with(@href,"https://profiles.google.com/")]/..//text()[contains(.,"@")]',
   ['photos']
 );
